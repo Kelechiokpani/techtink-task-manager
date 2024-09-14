045933a8216e705968ca5ccc1332e1595824274c
mac <emmanuelkelechi@verydeeptech.com>
2024-09-14 14:27:33 +0100

    Initial commit

--- app/utils/TimeFormatter.tsx
+++ app/utils/TimeFormatter.tsx
@@ -6,9 +6,10 @@ import moment from 'moment-timezone';
 const TimeDisplay = ({ createdAt }: { createdAt: string | Date }) => {
       const userTimeZone = moment.tz.guess();
       const createdAtLocal = moment(createdAt).tz(userTimeZone).format('YYYY-MM-DD HH:mm:ss');
+
       return (
             <p className="flex">
-                <p className="text-sm">Completed at: {createdAtLocal}</p>
+                <span className="text-sm">Completed at: {createdAtLocal}</span>
             </p>
 
       );
